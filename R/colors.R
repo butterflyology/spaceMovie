5b7a2e2d1e193b6d65d2daad2dca5e303e832a33
Chris Hamm <cahamm@ucdavis.edu>
2017-01-13 11:15:35 -0800

    added docs and metadata

--- R/colors.R
+++ R/colors.R
@@ -1,13 +1,14 @@
 #' Complete list of palettes
 #'
-#' Use \code{\link{wes_palette}} to construct palettes of desired length.
+#' Use \code{\link{SW_palette}} to construct palettes of desired length.
 #'
 #' @export
-space_movie <- list(
+SW_palettes <- list(
+  Main = c("#E5B13A", "#4BD5EE"),
   Boba = c("#546758", "#5A685D", "#926844", "#8E595A"),
   Kanan = c("#9C8412", "#6C571B", "#373A36"),
   Hera = c("#C5B783", "#C4B000", "#653819", "#EAAA00"),
-  Zeb = c("#201547", "#594A25", "512A44", "#F1C400", "#41273B", "#A07400"),
+  Zeb = c("#201547", "#594A25", "#512A44", "#F1C400", "#41273B", "#A07400"),
   Sabine = c("#E57200", "#651D32", "#25282A", "#5C462B", "#5B3427", "#41273B"),
   Chopper = c("#BE4D00", "#FED141", "#AEA79F", "#747474"),
   Ezra = c("#BE4D00", "#FFC72C", "#7D3F16", "#4F2C1D"),
@@ -27,7 +28,7 @@ space_movie <- list(
 #'   \code{Boba}, \code{Kanan},  \code{Hera},
 #'   \code{Zeb}, \code{Sabine},  \code{Chopper}, \code{Ezra},
 #'   \code{Inquisitor},  \code{Kallus} , \code{AT_DP} ,
-#'   \code{Tank_Transport_pilot}
+#'   \code{Tank_Transport_pilot}, \code{Main},
 #' @param type Either "continuous" or "discrete". Use continuous if you want
 #'   to automatically interpolate between colours.
 #' @return A vector of colours.
@@ -39,10 +40,10 @@ space_movie <- list(
 #' SW_palette("Hera")
 #' SW_palette("Sabine", 3)
 #'
-#' # If you need more colours than normally found in a palette, you
-#' # can use a continuous palette to interpolate between existing
-#' # colours
-#' pal <- wes_palette(21, name = "Zissou", type = "continuous")
+#' If you need more colours than normally found in a palette, you
+#' can use a continuous palette to interpolate between existing
+#' colours
+#' pal <- sw_palette(21, name = "Boba", type = "continuous")
 #' image(volcano, col = pal)
 SW_palette <- function(name, n, type = c("discrete", "continuous")) {
   type <- match.arg(type)
@@ -76,10 +77,10 @@ print.palette <- function(x, ...) {
         ylab = "", xaxt = "n", yaxt = "n", bty = "n")
 
   rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
-  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 2, family = "sans")
 }
 
 #' heatmap
 #'
 #' A heatmap example
-"heatmap"
+# "heatmap"
