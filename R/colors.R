d301c3f139a8e2c97867779192678a6b54666d1a
Chris Hamm <cahamm@ucdavis.edu>
2017-01-13 13:22:30 -0800

    completed README

--- R/colors.R
+++ R/colors.R
@@ -77,7 +77,7 @@ print.palette <- function(x, ...) {
         ylab = "", xaxt = "n", yaxt = "n", bty = "n")
 
   rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
-  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 2, family = "sans")
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1.5, family = "sans")
 }
 
 #' heatmap
